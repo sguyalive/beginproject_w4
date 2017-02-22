4d236fc0ad9a2be922b0cf714df4e66e04258606
sguyalive <fah-tvxq@live.com>
2017-02-22 15:04:53 +0700

    up

---
+++ marniacoffee/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>MARNIA COFFEE</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+</head>
+<body>
+
+    <div id="wrapper">
+         <button class="button" onclick="location.href='menu.php';">สั่งเครื่องดื่ม</button>
+    </div>
+
+</body>
+</html>
---
+++ marniacoffee/menu.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>MARNIA COFFEE</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+</head>
+<body>
+
+    <div id="wrapper-menu">
+    	<div class="btn-b1">
+    		<button class="btnmenu"></button>
+    			<!-- The Modal -->
+				<div class="btnmenu" class="modal">
+				  <!-- Modal content -->
+				  <div class="modal-content">
+				    <span class="close">&times;</span>
+				    <p>Some text in the Modal..</p>
+				  </div>
+				</div>
+
+    		<button class="btnmenu2"></button>
+    		<button class="btnmenu3"></button>
+    	</div>
+    	<div class="btn-b2">
+    		<button class="btnmenu4"></button>
+    		<button class="btnmenu5"></button>
+    		<button class="btnmenu6"></button>
+    	</div>
+    </div>
+
+
+
+</body>
+
+</html>
